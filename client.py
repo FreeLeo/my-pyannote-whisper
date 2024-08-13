666cc7989bcb2191d7c15075042583011c4f7e57
lizhen <>
2024-08-13 14:55:40 +0800

    add cuda

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from pyannote.audio import Pipeline
 from pyannote_whisper.utils import diarize_text
+import torch
 
 from datetime import datetime
 print(f"start {datetime.now()}")
@@ -8,5 +9,6 @@ pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-3.1",
     use_auth_token="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",
 )
+pipline.to(torch.device("cuda"))
 diarization_result = pipeline("data/afjiv.wav")
 print(f"end {datetime.now()}")
